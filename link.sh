8dce4b9696765d8888589b27d1a8b027f4085aed
mzumi <masazumi.m@gmail.com>
2015-09-05 20:49:07 +0900

    修正

---
+++ link.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ln -sf ~/dotfiles/atom ~/.atom
+ln -sf ~/dotfiles/brewfile ~/.brewfile
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/zshrc ~/.zshrc
+ln -sf ~/dotfiles/zshrc.orig ~/.zshrc.orig
+
