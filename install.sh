3250708ebcd721b3d9ceef874a145fdd40dc310e
mzumi <masazumi.m@gmail.com>
2020-08-14 19:09:45 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@ sudo apt-get -y install docker-compose
 
 # Settings for golang
 git clone https://github.com/syndbg/goenv.git ~/.goenv
+
+echo 'export GOENV_ROOT="$HOME/.goenv"' >> ~/.bash_profile
+echo 'export PATH="$GOENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(goenv init -)"' >> ~/.bash_profile
